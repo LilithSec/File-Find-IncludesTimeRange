8f89118617f40e596f6c071cb81f35213454cbab
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-02-26 22:46:49 -0600

    woot! all works now

--- lib/File/Find/IncludesTimeRange.pm
+++ lib/File/Find/IncludesTimeRange.pm
@@ -3,50 +3,155 @@ package File::Find::IncludesTimeRange;
 use 5.006;
 use strict;
 use warnings;
+use Time::Piece;
 
 =head1 NAME
 
-File::Find::IncludesTimeRange - The great new File::Find::IncludesTimeRange!
+File::Find::IncludesTimeRange - Takes a array of time stamped items(largely meant for use with files) returns ones that include the specified time range.
 
 =head1 VERSION
 
-Version 0.01
+Version 0.0.1
 
 =cut
 
-our $VERSION = '0.01';
-
+our $VERSION = '0.0.1';
 
 =head1 SYNOPSIS
 
-Quick summary of what the module does.
-
-Perhaps a little code snippet.
-
     use File::Find::IncludesTimeRange;
+    usexs Time::Piece;
+    use Data::Dumper;
 
-    my $foo = File::Find::IncludesTimeRange->new();
-    ...
+    my @files=(
+        'daemonlogger.1677468390.pcap',
+        'daemonlogger.1677468511.pcap',
+        'daemonlogger.1677468632.pcap',
+        'daemonlogger.1677468753.pcap',
+        'daemonlogger.1677468874.pcap',
+        'daemonlogger.1677468995.pcap',
+        'daemonlogger.1677469116.pcap',
+        'daemonlogger.1677469237.pcap',
+        'daemonlogger.1677469358.pcap',
+        'daemonlogger.1677469479.pcap',
+        'daemonlogger.1677469600.pcap',
+        'daemonlogger.1677469721.pcap',
+        );
 
-=head1 EXPORT
+    print Dumper(\@files);
 
-A list of functions that can be exported.  You can delete this section
-if you don't export anything, such as for a purely object-oriented module.
+    my $start=Time::Piece->strptime('1677468620', '%s');
+    my $end=Time::Piece->strptime(  '1677468633', '%s');
 
-=head1 SUBROUTINES/METHODS
+    my $found=File::Find::IncludesTimeRange->find(
+                                                  items=>\@files,
+                                                  start=>$start,
+                                                  end=>$end,
+                                                  regex=>'(?<timestamp>\d\d\d\d\d\d+)(\.pcap|(?<subsec>\.\d+)\.pcap)$',
+                                                  strptime=>'%s',
+                                                 );
 
-=head2 function1
+    print Dumper($found);
+
+=head1 SUBROUTINES
+
+=head2 find
 
 =cut
 
-sub function1 {
-}
+sub find {
+	my ( $blank, %opts ) = @_;
 
-=head2 function2
+	if ( !defined( $opts{start} ) ) {
+		die('$opts{start} is undef');
+	}
+	elsif ( !defined( $opts{end} ) ) {
+		die('$opts{end} is undef');
+	}
+	elsif ( !defined( $opts{items} ) ) {
+		die('$opts{items} is undef');
+	}
+	elsif ( ref( $opts{start} ) ne 'Time::Piece' ) {
+		die('$opts{start} is not a Time::Piece object');
+	}
+	elsif ( ref( $opts{end} ) ne 'Time::Piece' ) {
+		die('$opts{end} is not a Time::Piece object');
+	}
+	elsif ( ref( $opts{items} ) ne 'ARRAY' ) {
+		die('$opts{items} is not a ARRAY');
+	}elsif ( $opts{start} > $opts{end} ) {
+		die('$opts{start} is greater than $opts{end}');
+	}
 
-=cut
+	if (!defined($opts{regex})) {
+		$opts{regex}='(?<timestamp>\d\d\d\d\d\d+)(\.pcap|(?<subsec>\.\d+)\.pcap)$';
+	}
 
-sub function2 {
+	my $start = $opts{start}->epoch;
+	my $end   = $opts{end}->epoch;
+
+	my $found          = {};
+	my $timestamp_save = {};
+	foreach my $item ( @{ $opts{items} } ) {
+		if ( $item =~ /$opts{regex}/ ) {
+			my $subsec        = '';
+			my $timestamp_raw = $+{timestamp};
+			if ( defined( $+{subsec} ) ) {
+				$subsec = $+{subsec};
+			}
+
+			my $timestamp;
+			eval { $timestamp = Time::Piece->strptime( $timestamp_raw, $opts{strptime} ); };
+			if ( !$@ && defined($timestamp) ) {
+				my $full_timestamp = $timestamp->epoch . $subsec;
+				if ( !defined( $found->{$full_timestamp} ) ) {
+					$found->{$full_timestamp}          = [];
+					$timestamp_save->{$full_timestamp} = $timestamp;
+				}
+				push( @{ $found->{$full_timestamp} }, $item );
+			}
+		}
+	}
+
+	my @found_timestamps = sort( keys( %{$found} ) );
+	my $previous_timestamp;
+	my $previous_found;
+	my $previous_t;
+	my @timestamp_to_return;
+	foreach my $current_timestamp (@found_timestamps) {
+		my $t = $timestamp_save->{$current_timestamp};
+
+		if ( ( $opts{start} <= $t ) && ( $t <= $opts{end} ) ) {
+			push( @timestamp_to_return, $current_timestamp );
+
+			# if we find one that it is between, but not equal, then add the previous as that contains the start
+			if ( defined($previous_timestamp) && !$previous_found && ( $opts{start} != $t ) ) {
+				$previous_found = 1;
+				push( @timestamp_to_return, $previous_timestamp );
+			}elsif (!$previous_found && ( $opts{start} == $t ) ) {
+				$previous_found = 1;
+			}
+		}
+
+		# if the time period falls between when two files was created, we will find 
+		if (!$previous_found && defined($previous_timestamp) && ( $opts{end} < $t )) {
+			$previous_found=1;
+			push( @timestamp_to_return, $previous_timestamp );
+		}
+
+		$previous_timestamp = $current_timestamp;
+		$previous_t         = $t;
+	}
+
+	my $to_return=[];
+	# the second sort is needed as if 
+	foreach my $item (sort(@timestamp_to_return)) {
+		foreach my $file (@{ $found->{$item} }) {
+			push(@{ $to_return }, $file);
+		}
+	}
+
+	return $to_return;
 }
 
 =head1 AUTHOR
@@ -102,4 +207,4 @@ This is free software, licensed under:
 
 =cut
 
-1; # End of File::Find::IncludesTimeRange
+1;    # End of File::Find::IncludesTimeRange
