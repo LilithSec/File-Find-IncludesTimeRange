934fa198ee8524c1874dcf8394492ae0e13d63b1
Zane C. Bowers-Hadley <vvelox@vvelox.net>
2023-08-11 16:08:32 -0500

    note the changes and bump to 0.2.0

--- lib/File/Find/IncludesTimeRange.pm
+++ lib/File/Find/IncludesTimeRange.pm
@@ -11,11 +11,11 @@ File::Find::IncludesTimeRange - Takes a array of time stamped items(largely mean
 
 =head1 VERSION
 
-Version 0.1.1
+Version 0.2.0
 
 =cut
 
-our $VERSION = '0.1.1';
+our $VERSION = '0.2.0';
 
 =head1 SYNOPSIS
 
